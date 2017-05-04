eda90d54b210f3464e1976e8c82194ae5f21eec3
FirstAtomsk <alxbryer@gmail.com>
2017-05-04 15:00:37 +0300

    Ветка с инкапсуляцией ID

---
+++ ConsoleApp1/IDclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ID
+    {
+        public string name;
+        public int weight;
+        public string city;
+        public string login;
+        public string pass;
+
+        public ID (string _name, int _weight, string _city, string _login, string _pass )
+        {
+            name = _name;
+            weight = _weight;
+            city = _city;
+            login = _login;
+            pass = _pass;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ID alex = new ID("alex", 60, "Obninsk", "alex", "12345");
+            ID ilya = new ID("ilya", 70, "Moscow", "ilya", "12345");
+
+            Console.WriteLine(alex.weight);
+            Console.ReadLine();
+        }
+    }
+}
