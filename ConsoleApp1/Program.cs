34e44a88525c655a4224d73c2c6ac6eed8529dd7
FirstAtomsk <alxbryer@gmail.com>
2017-05-04 15:53:30 +0300

    Сделал аутентификацию по массиву, работает.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,11 +12,45 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            ID alex = new ID("alex", 60, "Obninsk", "alex", "12345");
+            ID alex = new ID("alex", 60, "Obninsk", "alex", "12345");           /*это у меня такая БД*/
             ID ilya = new ID("ilya", 70, "Moscow", "ilya", "12345");
 
-            Console.WriteLine(alex.weight);
+            List<ID> idList = new List<ID>();           /*вывожу мою БД в массив*/
+            idList.Add(alex);
+            idList.Add(ilya);
+
+            Console.WriteLine("Введите свой логин");            /*проверяю логин по массиву*/
+            string loginow = Console.ReadLine();
+
+            foreach ( ID i in idList)
+            {
+                if (i.login == loginow)
+                {
+                    Console.WriteLine("Логин принят, введите пароль");
+                    passcheck:
+                    string passnow = Console.ReadLine();
+                    if(i.pass == passnow)
+                    {
+                        Console.WriteLine("Аутентификация успешна.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный пароль, попробуйте ещё раз.");
+                        goto passcheck;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Данное имя пользователя не зарегистрировано");
+                }
+            
+            }
+
             Console.ReadLine();
         }
+
+
+        
     }
 }
