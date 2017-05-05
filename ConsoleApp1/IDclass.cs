95e90e8d99d66cc64523968618c4bcaa89c1d559
FirstAtomsk <alxbryer@gmail.com>
2017-05-05 16:47:33 +0300

    Сделал рейтинг, надо доработать плавность навигации, выстраивание по позиции и пр.

--- ConsoleApp1/IDclass.cs
+++ ConsoleApp1/IDclass.cs
@@ -13,14 +13,18 @@ namespace ConsoleApp1
         public string city;
         public string login;
         public string pass;
+        public int BP_res;
+        public int BP_place;
 
-        public ID (string _name, int _weight, string _city, string _login, string _pass )
+        public ID (string _name, int _weight, string _city, string _login, string _pass, int _BP_res, int _BP_place)
         {
             name = _name;
             weight = _weight;
             city = _city;
             login = _login;
             pass = _pass;
+            BP_res = _BP_res;
+            BP_place = _BP_place;
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,8 +13,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            ID alex = new ID("Брайер Алексей", 60, "Обнинск", "Alex", "12345");           /*это у меня такая "БД"*/
-            ID ilya = new ID("Ситников Илья", 70, "Москва", "BigRichDick", "12345");
+            ID alex = new ID("Брайер Алексей", 60, "Обнинск", "Alex", "12345", 60, 1);           /*это у меня такая "БД"*/
+            ID ilya = new ID("Ситников Илья", 70, "Москва", "BigRichDick", "12345", 0, 100 );
+            ID homer = new ID("Гомер Симпсон", 120, "Спрингфилд", "Homer", "Donuts", 20, 4);
+            ID simp = new ID("Мамин Симпатяга", 40, "Зажопинск", "Simpa98", "maman", 25, 3);
+            ID brod = new ID("Папин Бродяга", 65, "Зажопинск", "CoolGuy", "pahan", 50, 2);
 
             List<ID> idList = new List<ID>();           /*заливаю БД в массив*/
             idList.Add(alex);
@@ -25,7 +28,7 @@ namespace ConsoleApp1
 
             int token = 1;          /*это я так чекаю прохождение массива без совпадений - связка a1 */
 
-            ID usernow = new ID("0", 0, "0", "0", "0");
+            ID usernow = new ID("0", 0, "0", "0", "0", 0, 101);
 
             foreach (ID i in idList)            /*собственно, аутентификация, шарю массив*/
             {
@@ -53,6 +56,10 @@ namespace ConsoleApp1
             if (token == 1) Console.WriteLine("Данное имя пользователя не найдено");            /*связка a1*/
 
             Profile (usernow.name, usernow.city, usernow.weight);
+
+            NewRecordFunc(alex, homer, simp, brod, ilya, usernow);
+
+            Console.ReadLine();
             
         }
 
@@ -73,8 +80,60 @@ namespace ConsoleApp1
 
             Console.ReadLine();
 
+           
+
         }
 
+        static void NewRecordPage()             /*страница выбор упражнения новый рекорд*/         
+        {
+            //Console.WriteLine();
+            //Console.WriteLine(" 1 - Bench press.");
+            //Console.WriteLine(" 2 - Dead lift.");
+            //string rec_choice = Console.ReadLine();
+            //if (rec_choice == "1")
+            //{
+            //    NewRecordFunc(/*bench press*/);
+            //}
+            //else if (rec_choice == "2")
+            //{
+            //    NewRecordFunc(/*dead lift*/)
+            //}
+            //else Console.WriteLine("Неизвестный запрос");
 
+            
+        }
+
+        static void NewRecordFunc(ID alex, ID homer, ID simp, ID brod, ID ilya, ID usernow)             /*функциональное исполнение нового рекорда*/
+        {
+            Console.WriteLine("Введите свой результат");
+            usernow.BP_res = Int32.Parse(Console.ReadLine());
+
+            List<ID> BenchPressList = new List<ID>();
+
+            BenchPressList.Add(alex);
+            BenchPressList.Add(homer);
+            BenchPressList.Add(simp);
+            BenchPressList.Add(brod);
+            BenchPressList.Add(ilya);
+
+
+            foreach (ID i in BenchPressList)
+            {
+                if (usernow.BP_res > i.BP_res)
+                {
+                    if (usernow.BP_place > i.BP_place) usernow.BP_place = i.BP_place;
+                    i.BP_place++;
+                }
+            }
+
+            foreach (ID i in BenchPressList)
+            {
+                Console.WriteLine(i.name + i.BP_place + " " + i.BP_res);
+            }
+                
+        }
+
+        
+         
     }
 }
